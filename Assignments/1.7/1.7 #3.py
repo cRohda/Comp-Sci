b8b7dfe0dc657557ee42ffbdaca24053a873b2dd
cRohda <cormacrohda@gmail.com>
2021-10-20 10:30:25 -0400

    Finished 1.7 #3 & #4, with comments

--- Assignments/1.7/1.7 #3.py
+++ Assignments/1.7/1.7 #3.py
@@ -1,3 +1,10 @@
-# **REDO THIS, DOESN'T COUNT CAUSE WE HAVEN'T LEARNED BRACKETS**
-phrase = input('Enter a word or sentence: ')  # Gets the users input of a sentence
-print(phrase[::2])  # Prints the users input, but only printing every-other character
+phrase = list(input('Enter a word or sentence: '))  # Gets the users input of a sentence
+output = ""  # Creates the variable output, and makes it a empty string
+# creates a range with the length of the user's phrase
+for i in range(len(phrase)):  # Loops through assigning i to each num in this range
+    if i % 2 == 0:  # if the number of i (index of list) is evenly divisible by 2
+        print(phrase[i])  # print the character who's index is divisible by 2
+
+# **Do it in 2 lines?**
+# phrase = input('Enter a word or sentence: ')
+# print(phrase[::2])
--- Assignments/1.7/1.7 #4.py
+++ Assignments/1.7/1.7 #4.py
@@ -1,2 +1,24 @@
-userList = list(input('Enter a list: '))
-print(userList)
+List = [2, 324.3214, 'a', 'b', True, [2, 5]]  # The list in question
+x = 0  # creates x, counts up each time through to know when to stop counting the types
+string = 0
+Boolean = 0
+Integer = 0  # Variables to count the number of each type
+Decimal = 0
+Group = 0
+for i in List:  # For each value in list
+    if type(List[x]) == str:  # if the type is a string
+        string += 1  # Add one to the string count
+    elif type(List[x]) == bool:  # if the type is a boolean
+        Boolean += 1  # add one to the boolean count
+    elif type(List[x]) == int:  # this same process repeats for each type
+        Integer += 1
+    elif type(List[x]) == float:
+        Decimal += 1
+    elif type(List[x]) == list:
+        Group += 1
+    if x <= len(List):  # if x is less than/equal too the length of the list
+        x += 1  # add one fo x
+    else:  # if x is more than the length of the list (this means it's tried every value)
+        break  # end the loops
+# Prints the number of each type
+print(f'Strings: {string}\nBooleans: {Boolean}\nIntegers: {Integer}\nFloats: {Decimal}\nLists: {Group}')

ec77df521f8d154ac689e0a8f5310dbacdc48f11
cRohda <cormacrohda@gmail.com>
2021-12-03 10:30:43 -0500

    Updated 2.2.5

--- Assignments/2.2/C5.py
+++ Assignments/2.2/C5.py
@@ -3,5 +3,5 @@ my_string = "incredible"  # Create string
 s3 = (my_string*3)  # Make new string that is 3x the first string
 print(s3)  # Print new string
 
-cred3 = (my_string[2:6]*3)  # Make new string which is cred 3x, (indies 2,3,4,5)
-print(cred3)  # Print the new string
+cred3 = (s3[2:6]*3)  # Make new string which is cred 3x, (indies 2,3,4,5)
+print(cred3)  # Prinet the new string

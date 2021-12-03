bbfe766b6b39c7251608df3a0f5fece771a48eed
cRohda <cormacrohda@gmail.com>
2021-12-03 10:43:20 -0500

    Spelling Error in Comment Fixed

--- Assignments/2.2/C5.py
+++ Assignments/2.2/C5.py
@@ -4,4 +4,4 @@ s3 = (my_string*3)  # Make new string that is 3x the first string
 print(s3)  # Print new string
 
 cred3 = (s3[2:6]*3)  # Make new string which is cred 3x, (indies 2,3,4,5)
-print(cred3)  # Prinet the new string
+print(cred3)  # Print the new string

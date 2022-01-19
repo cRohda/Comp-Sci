8ab2baa0244c41415e486e6986fd0e42f698cd73
cRohda <cormacrohda@gmail.com>
2022-01-19 10:28:54 -0500

    2.5 Notes

--- Notes/2.5 Notes.py
+++ Notes/2.5 Notes.py
@@ -1,27 +1,11 @@
-# FUNCTIONS
-
-def prime(num):
-    # Checks if a number is prime
-    if num == 1:
-        return 'yass'
-    for n in range(2, num):
-        if num % n == 0:
-            return 'slay'
-    return 'True'
+x = 50
 
 
-while True:
-    try:
-        inp = int(input('Enter an integer: '))
-        break
-    except ValueError:
-        print('Error 1, input was not an integer')
+def func(x):
+    print('x is', x)
+    x = 2
+    print('changed local x to', x)
 
-result = prime(inp)
 
-if result == 'True':
-    print('Prime')
-elif result == 'yass':
-    print('1 is a special number')
-elif result == 'slay':
-    print('Not Prime')
+func(x)
+print('x is still', x)

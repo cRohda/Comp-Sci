82c041fc08830a61aabff13d5f2ff7e17266c660
cRohda <cormacrohda@gmail.com>
2022-01-21 14:26:45 -0500

    added notes of what code mimics problem 3

--- Assignments/Midterm Projects/A-5.py
+++ Assignments/Midterm Projects/A-5.py
@@ -1,6 +1,7 @@
 list1 = [15, -2, 10, -5, -4, 6, 3, -1, -8, 13, -12, -11, 7, 14]  # Base list
 even_list = []  # Creates blank list
 
+# Problem 3 code
 for i in list1:  # Iterates through list1 assigning i to the next value each time through
     if i % 2 == 0:  # If 'i' is divisible by 2 (even) then:
         even_list.append(i)  # Add the value of 'i' to a new list 'even_list'
@@ -8,6 +9,7 @@ print(even_list)  # Print the value of 'even_list'
 
 # Using option 2
 
+# New code
 sorted_list = []  # Creates empty list 'sorted_list'
 
 while len(even_list) > 0:  # Loops as long as there are values in even_list
--- Assignments/Midterm Projects/B+4.py
+++ Assignments/Midterm Projects/B+4.py
@@ -1,11 +1,13 @@
 list1 = [15, -2, 10, -5, -4, 6, 3, -1, -8, 13, -12, -11, 7, 14]  # Base list
 even_list = []  # Creates blank list
 
+# Problem 3 code
 for i in list1:  # Iterates through list1 assigning i to the next value each time through
     if i % 2 == 0:  # If 'i' is divisible by 2 (even) then:
         even_list.append(i)  # Add the value of 'i' to a new list 'even_list'
 print(even_list)  # Print the value of 'even_list'
 
+# New code
 smallest = even_list[0]  # Set smallest variable to the 0 index of even_list
 for x in even_list:  # Iterates through even_list assigning 'x' to the next value each time through
     if x < smallest:  # if 'x' is less than the current value of 'smallest'
